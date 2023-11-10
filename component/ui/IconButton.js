53102837f16cff998f31fcc312110f8965a00a03
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-10 08:35:29 +0100

    chore: update the icon button and install the RN navigation packages

--- component/ui/IconButton.js
+++ component/ui/IconButton.js
@@ -1,7 +1,27 @@
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 
-function IconButton() {
-  return <Pressable> </Pressable>;
+import { Ionicons } from "@expo/vector-icons";
+
+function IconButton({ size, color, icon, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.button, pressed && styles.buttonPresed]}
+    >
+      <Ionicons size={size} name={icon} color={color} />
+    </Pressable>
+  );
 }
 
 export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonPresed: {
+    opacity: 0.7,
+  },
+});
