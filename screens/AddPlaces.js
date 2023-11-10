1357844318807149ff80fa3e7340d43fd61f5b05
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-10 09:57:24 +0100

    feat: add addPlace form

--- screens/AddPlaces.js
+++ screens/AddPlaces.js
@@ -1,11 +1,38 @@
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../utils/color";
 
 function AddPlaces() {
   return (
-    <View>
-      <Text> Add a new place </Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}> Name </Text>
+        <TextInput style={styles.input} />
+      </View>
+    </ScrollView>
   );
 }
 
 export default AddPlaces;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 8,
+  },
+
+  inputContainer: {
+    rowGap: 6,
+  },
+
+  label: {
+    color: Colors.primary500,
+    fontWeight: "bold",
+  },
+
+  input: {
+    backgroundColor: Colors.primary100,
+    flex: 1,
+    paddingVertical: 6,
+    paddingHorizontal: 4,
+  },
+});
