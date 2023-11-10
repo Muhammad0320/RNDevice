acb57974bc1725d76a10a5d71dc0aa0b9e127fa9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-10 10:44:35 +0100

    chore: done ios camera permission

--- component/places/ImagePicker.js
+++ component/places/ImagePicker.js
@@ -1,18 +1,52 @@
-import { launchCameraAsync } from "expo-image-picker";
-import { Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { Alert, Button, Text, View } from "react-native";
 
 function ImagePicker() {
+  const [cameraPermisssionInfo, requestPermission] = useCameraPermissions();
+
+  const verifyPermission = async () => {
+    if (cameraPermisssionInfo.status === PermissionStatus.UNDETERMINED) {
+      const PermissionResnponse = await requestPermission();
+
+      return PermissionResnponse.granted;
+    }
+
+    if (cameraPermisssionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permission",
+        "You need to grant camera permission to use this app effectively"
+      );
+
+      return false;
+    }
+
+    return true;
+  };
+
   const handleTakeImage = async () => {
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       quality: 0.6,
       aspect: [16, 9],
     });
+    console.log(image);
   };
 
   return (
     <View>
-      <View> </View>
+      <View>
+        <Text> Odeh </Text>
+      </View>
 
       <Button title="Take Image" onPress={handleTakeImage} />
     </View>
