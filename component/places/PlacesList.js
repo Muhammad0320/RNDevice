02c67596b536b28848337902afc650c2eefa5974
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-10 07:15:15 +0100

    chore: add places list fallback

--- component/places/PlacesList.js
+++ component/places/PlacesList.js
@@ -1,6 +1,17 @@
-import { FlatList } from "react-native";
+import { FlatList, Text, View } from "react-native";
 
 function PlacesList({ places }) {
+  if (!places.length) {
+    return (
+      <View>
+        <Text>
+          {" "}
+          You have no place right now! - start by adding some places{" "}
+        </Text>
+      </View>
+    );
+  }
+
   return <FlatList data={places} />;
 }
 
