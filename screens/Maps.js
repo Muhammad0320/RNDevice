5470001ae0c4f7cf7d8fa474339bdf49afb05ad2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-11 10:05:45 +0100

    chore: sent data when to the add place when moving back

--- screens/Maps.js
+++ screens/Maps.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import IconButton from "../component/ui/IconButton";
 
-function Maps() {
+function Maps({ navigation }) {
   // 37.77472666989382, -122.44141910593166
 
   const [selectedLocation, setSelectedLocation] = useState(null);
@@ -26,10 +27,41 @@ function Maps() {
       initialRegion={region}
       onPress={handleClickMap}
     >
-      {selectedLocation && <Marker coordinate={{ ...selectedLocation }} />}
+      {selectedLocation && (
+        <Marker title="Picked location" coordinate={{ ...selectedLocation }} />
+      )}
     </MapView>
   );
 }
+
+const handlePickedLocation = useCallback(
+  () => () => {
+    if (!selectedLocation) {
+      Alert.alert(
+        "No location picked!",
+        "You have to pick a location - by tapping on the map - first"
+      );
+
+      return;
+    }
+
+    navigation.navigate("AddPlace", { ...selectedLocation });
+  },
+  [navigation, selectedLocation]
+);
+
+useLayoutEffect(() => {
+  navigation.setOptions({
+    headerRight: ({ tintColor }) => (
+      <IconButton
+        icon="save"
+        color={tintColor}
+        size={24}
+        onPress={handlePickedLocation}
+      />
+    ),
+  });
+}, [navigation, handlePickedLocation]);
 
 export default Maps;
 
