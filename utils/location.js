e248cdd66e04fdb0ea15bcc1ad8d0ade033abde3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-11 08:00:50 +0100

    chore: complete the get location login

--- utils/location.js
+++ utils/location.js
@@ -1,5 +1,10 @@
 GOOGLE_API_KEY = "AIzaSyCFe5gDVVJtzXrpD8JxPJtK6m9_s9rcWPk";
 
-function getLocationPreview() {}
+function getLocationPreview(lat, lng) {
+  const location = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=600x300&maptype=roadmap
+    &markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+
+  return location;
+}
 
 export default getLocationPreview;
