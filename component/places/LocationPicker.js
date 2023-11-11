766b6c1a4d7345b57ad82def55bd4ef70eb7d800
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-11 04:27:29 +0100

    chore: add location picker

--- component/places/LocationPicker.js
+++ component/places/LocationPicker.js
@@ -1,7 +1,28 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Colors } from "../../utils/color";
 
 function LocationPicker() {
   return <View></View>;
 }
 
 export default LocationPicker;
+
+const styles = StyleSheet.create({
+  previewContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary500,
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    borderRadius: 6,
+  },
+
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
