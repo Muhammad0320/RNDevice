f6ef32d2806e32743547026bde0663202a6898b0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-11 04:10:19 +0100

    chore: code up outline button styles

--- component/ui/OutLinedButton.js
+++ component/ui/OutLinedButton.js
@@ -1,17 +1,46 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "../../utils/color";
 
-function Button({ icon, onPress, children }) {
+function OutlinedButton({ icon, onPress, children }) {
   return (
-    <Pressable onPress={onPress}>
-      <View>
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.container, pressed && styles.pressed]}
+    >
+      <View style={styles.textIcon}>
         <Ionicons name={icon} color={Colors.primary500} size={16} />
-        <Text> {children} </Text>
+        <Text style={styles.text}> {children} </Text>
       </View>
     </Pressable>
   );
 }
 
-export default Button;
+export default OutlinedButton;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    borderColor: Colors.primary500,
+    borderWidth: 2,
+    backgroundColor: "transparent",
+  },
+
+  pressed: {
+    opacity: 0.7,
+  },
+
+  textIcon: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    columnGap: 6,
+  },
+
+  text: {
+    color: Colors.primary500,
+    fontSize: 16,
+  },
+});
