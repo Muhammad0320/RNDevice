f8ae56af46351b0f9a82f90235c529cd3c93ffd2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-11 09:14:50 +0100

    chore: setup the maps comp

--- screens/Maps.js
+++ screens/Maps.js
@@ -1,7 +1,16 @@
-import { View } from "react-native";
+import MapView from "react-native-maps";
 
 function Maps() {
-  return <View></View>;
+  // 37.77472666989382, -122.44141910593166
+
+  const region = {
+    latitude: 37.77472666989382,
+    longitude: -122.44141910593166,
+    latitudeDelta: 0.0921,
+    longitudeDelta: 0.0421,
+  };
+
+  return <MapView initialRegion={region}> </MapView>;
 }
 
 export default Maps;
