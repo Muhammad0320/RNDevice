cb23a49b852bc9d4402c23821f1f6451620e91e8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-12 05:23:56 +0100

    chore: style and apply the style to button

--- component/ui/Button.js
+++ component/ui/Button.js
@@ -1,7 +1,38 @@
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { Colors } from "../../utils/color";
 
-function Button() {
-  return <Pressable> </Pressable>;
+function Button({ children, onPress }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}> {children} </Text>
+    </Pressable>
+  );
 }
 
 export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary500,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 6,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOffset: { height: 1, width: 1 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+  },
+
+  pressed: {
+    opacity: 0.7,
+  },
+
+  text: {
+    color: Colors.primary50,
+    fontSize: 16,
+  },
+});
