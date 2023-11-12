2903ddda672dbeba3df50b01c1fc9c81e851989e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-12 07:35:52 +0100

    chore: navigate to all places when submitted

--- screens/AddPlaces.js
+++ screens/AddPlaces.js
@@ -1,74 +1,13 @@
-import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-import { Colors } from "../utils/color";
-import { useCallback, useState } from "react";
-import ImagePicker from "../component/places/ImagePicker";
-import LocationPicker from "../component/places/LocationPicker";
-import Button from "../component/ui/Button";
+import PlacesForm from "../component/places/PlacesForm";
 
-function AddPlaces() {
-  const [enteredTitle, setEnteredTitle] = useState("");
-
-  const [pickedLoction, setPickLocation] = useState(null);
-
-  const [pickImage, setPickImage] = useState("");
-
-  const handleTextInput = (enteredText) => {
-    setEnteredTitle(enteredText);
+function AddPlaces({ navigation }) {
+  const handleSaveData = (place) => {
+    navigation.navigate("AllPlaces", {
+      place,
+    });
   };
 
-  const handlePickedLocation = useCallback((location) => {
-    setPickLocation(location);
-  }, []);
-
-  const handlePcikedImage = (image) => {
-    setPickImage(image);
-  };
-
-  const handleSavePlace = () => {
-    console.log(enteredTitle);
-    console.log(pickedLoction);
-    console.log(pickImage);
-  };
-
-  return (
-    <ScrollView style={styles.form}>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}> Title </Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={handleTextInput}
-          value={enteredTitle}
-        />
-      </View>
-      <ImagePicker onPickImage={handlePcikedImage} />
-      <LocationPicker onPickLocation={handlePickedLocation} />
-      <Button onPress={handleSavePlace}> Add Place </Button>
-    </ScrollView>
-  );
+  return <PlacesForm onSaveData={handleSaveData} />;
 }
 
 export default AddPlaces;
-
-const styles = StyleSheet.create({
-  form: {
-    flex: 1,
-    padding: 10,
-    paddingHorizontal: 20,
-  },
-
-  inputContainer: {
-    rowGap: 6,
-  },
-
-  label: {
-    color: Colors.primary500,
-    fontWeight: "bold",
-  },
-
-  input: {
-    backgroundColor: Colors.primary100,
-    flex: 1,
-    paddingVertical: 6,
-    paddingHorizontal: 4,
-  },
-});
