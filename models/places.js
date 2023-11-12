dbb7592f8065888ae121aa25988d6d17a71ae7bc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-12 07:26:48 +0100

    chore: reconstruct the Place Class constructor

--- models/places.js
+++ models/places.js
@@ -1,6 +1,7 @@
 class Places {
-  constructor(title, address, imageUri) {
-    this.address = address;
+  constructor(title, imageUri, location) {
+    this.address = location.address;
+    this.coords = { lat: location.latitude, lng: location.longitude };
     this.title = title;
     this.imageUri = imageUri;
     this.id = `${Date.now()}-${Math.random()}`;
