081e0d52ae2234a18b5bdd6e17eb663a87740a7e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-12 08:23:14 +0100

    chore: add place item styles

--- component/places/placesItem.js
+++ component/places/placesItem.js
@@ -1,10 +1,11 @@
-import { Image, Pressable, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../utils/color";
 
 function PlacesItem({ place, onPress }) {
   const { imageUri, address, title } = place;
 
   return (
-    <Pressable onPress={onPress}>
+    <Pressable onPress={onPress} android_ripple={{ color: Colors.primary400 }}>
       <Image source={{ uri: imageUri }} />
 
       <View>
@@ -16,3 +17,39 @@ function PlacesItem({ place, onPress }) {
 }
 
 export default PlacesItem;
+
+const style = StyleSheet.create({
+  container: {
+    overflow: "hidden",
+    flexDirection: "row",
+    backgroundColor: Colors.primary500,
+    borderRadius: 6,
+    elevation: 4,
+    shadowOffset: { height: 1, width: 1 },
+    shadowColor: "#000",
+    shadowRadius: 4,
+    shadowOpacity: 0.5,
+    alignItems: "flex-start",
+    columnGap: 10,
+  },
+  pressed: {
+    opacity: 0.8,
+  },
+
+  info: {
+    flex: 2,
+    rowGap: 6,
+    paddingVertical: 10,
+  },
+  image: {
+    flex: 1,
+    height: 100,
+  },
+  title: {
+    color: Colors.gray700,
+    fontSize: 18,
+  },
+  address: {
+    color: Colors.gray700,
+  },
+});
