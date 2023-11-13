8a253be603759573b4fd51ee04a2aa9f87e3fea5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-13 04:10:47 +0100

    chore: create then sql func

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,33 @@
 import * as SQLITE from "expo-sqlite";
 
-const init = () => {};
+const database = SQLITE.openDatabase("places.db");
+
+const init = () => {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        ` CREATE TABLE IF NOT EXISTS places  (
+
+                    id INTERGER PRIMARY KEY NOT NULL,
+                    title TEXT NOT NULL,
+                    imageUri TEXT NOT NULL,
+                    address TEXT NOT NULL,
+                    lat REAL NOT NULL,
+                    lng REAL NOT NULL
+
+
+
+                ) `,
+        [],
+        () => {
+          resolve();
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+
+  promise();
+};
