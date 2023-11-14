be67e476bd5445c88678e1229064d403d27b48e8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-14 04:48:19 +0100

    chore: complete the page layout

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,7 +1,30 @@
-import { ScrollView } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import OutlinedButton from "../component/ui/OutlinedButton";
+import { useEffect } from "react";
 
-function PlaceDetails() {
-  return <ScrollView></ScrollView>;
+function PlaceDetails({ route }) {
+  const viewOnMapHandler = () => {};
+
+  const { placeId } = route.params;
+
+  useEffect(() => {
+    // logic for fetching current place data
+  }, [placeId]);
+
+  return (
+    <ScrollView>
+      <Image />
+      <View>
+        <View>
+          <Text> Address </Text>
+        </View>
+        <OutlinedButton icon="map" onPress={viewOnMapHandler}>
+          {" "}
+          View on Map{" "}
+        </OutlinedButton>
+      </View>
+    </ScrollView>
+  );
 }
 
 export default PlaceDetails;
